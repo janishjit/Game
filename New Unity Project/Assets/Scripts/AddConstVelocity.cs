191de0d788f630016a69e0143445ea27c3fabe7c
Ratish Juneja <69637888+ratish1112@users.noreply.github.com>
2021-01-04 18:13:19 -0800

    Player Mei hu

---
+++ New Unity Project/Assets/Scripts/AddConstVelocity.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddConstVelocity : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 v3force;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().velocity += v3force;
+    }
+}
---
+++ New Unity Project/Assets/Scripts/AddPlayerControlledVelocity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPlayerControlledVelocity : MonoBehaviour
+{
+
+    public float speed;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal"); 
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+
+        gameObject.transform.Translate(direction.normalized * Time.deltaTime * speed);
+    }
+}
