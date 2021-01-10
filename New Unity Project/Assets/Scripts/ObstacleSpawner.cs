c144ba8f70431ea710623324d81a7a4cc35bdcdf
janishjit <65305278+janishjit@users.noreply.github.com>
2021-01-09 19:27:51 -0800

    Obstacle Scripts added

---
+++ New Unity Project/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public GameObject[] obstaclePrefab;
+    public float spawnTime = 1;
+    private float timer = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > spawnTime){ 
+            int rand = Random.Range(0, obstaclePrefab.Length);
+
+            GameObject obs = Instantiate(obstaclePrefab[rand]);
+            obs.transform.position = transform.position + new Vector3(0, 0, 0);
+            Destroy(obs, 15);
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
+    }
+}
